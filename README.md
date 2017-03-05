af349c6c7e1a0007032b9bcfdf0f7d07e5ff8a93
Oleg Kulikov <oakulikov@yandex.ru>
2017-03-05 14:11:35 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Cmd::Interpreter - It's new $module
+
+# SYNOPSIS
+
+    use Cmd::Interpreter;
+
+# DESCRIPTION
+
+Cmd::Interpreter is ...
+
+# LICENSE
+
+Copyright (C) Oleg Kulikov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Oleg Kulikov <oakulikov@yandex.ru>
---
+++ lib/Cmd/Interpreter.pm
@@ -0,0 +1,39 @@
+package Cmd::Interpreter;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Cmd::Interpreter - It's new $module
+
+=head1 SYNOPSIS
+
+    use Cmd::Interpreter;
+
+=head1 DESCRIPTION
+
+Cmd::Interpreter is ...
+
+=head1 LICENSE
+
+Copyright (C) Oleg Kulikov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Oleg Kulikov E<lt>oakulikov@yandex.ruE<gt>
+
+=cut
+
